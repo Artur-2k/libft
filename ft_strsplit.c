4e2994e1181727eb265b56e03893f6c8c77c63ec
Artur Carvalho <miniruto.gameplays@gmail.com>
2024-04-17 20:11:19 +0100

    added ft_strsplit.c contains static count_words, word_lenght_free for later implementations.

---
+++ ft_strsplit.c
@@ -0,0 +1,79 @@
+#include "libft.h"
+
+static int ft_count_words(const char *str, char sep)
+{
+	int count;
+
+	if (!*str)
+		return (0);
+	count = 1;
+	while(*str)
+	{
+		if (*str == sep && *(str + 1) != 0)
+			count++;
+		str++;
+	}
+	return (count);
+}
+static size_t ft_word_lenght(const char *str, char sep)
+{
+	size_t len;
+
+	len = 0;
+	while(*str != sep)
+	{
+		len++;
+		str++;
+	}
+	return (len++);
+}
+
+static void ft_free(char **arr, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr[i]);
+}
+char **ft_split(const char *s, char c)
+{
+	char **sarr;
+	int i;
+	int words;
+	int start;
+	int wlen;
+
+	words = ft_count_words(s, c);
+	sarr = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!sarr)
+		return (NULL);
+	i = 0;
+	start = 0;
+	while (i < words)
+	{
+		wlen = ft_word_lenght(&s[start], c);
+		sarr[i] = ft_substr(s, start, wlen);
+		start += wlen + 1;
+		i++;
+	}
+	sarr[i] = NULL;
+	return (sarr);
+}
+
+/* #include <stdio.h>
+ int main() {
+    char str[] = "ola mundo maluco!";
+    char charset = ' ';
+    char **result = ft_split(str, charset);
+ 	printf("string:%s\n", str);
+    printf("Split result:\n");
+    for (int i = 0; result[i] != NULL; i++)
+    	printf("%s\n", result[i]);
+    ft_free(result, ft_count_words(str, charset) + 1);
+    return 0;
+} */
