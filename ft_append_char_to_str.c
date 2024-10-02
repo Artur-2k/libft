64f7d75e3abbfdeabecf481790dfa1188588fec5
Artur Carvalho <160274976+Artur-2k@users.noreply.github.com>
2024-10-02 15:37:05 +0100

    Added ft_append_char_to_str.c

---
+++ ft_append_char_to_str.c
@@ -0,0 +1,24 @@
+// Appends a char to a string keeping it null terminated.
+// Returns a new allocated string and frees the old one.
+char	*ft_append_char_to_str(char *str, char c)
+{
+	char	*nstr;
+	size_t			len;
+
+	len = 0;
+	if (str)
+		len = ft_strlen((const char *)str);
+	nstr = (char *)malloc(sizeof(char) * len + 2);
+	if (!nstr)
+		return (free(str), NULL);
+	len = 0;
+	while (str && str[len])
+	{
+		nstr[len] = str[len];
+		len++;
+	}
+	nstr[len] = c;
+	nstr[++len] = '\0';
+	free(str);
+	return (nstr);
+}
